dbb35f397cc6874dd1cdfe92fe4f445a50c4f9ce
Priyak Dey <priyak1991dey@gmail.com>
2025-11-06 23:30:21 +0530

    patching notes

--- algoexpert/src/main/java/com/priyakdey/arrays/MinRewards.java
+++ algoexpert/src/main/java/com/priyakdey/arrays/MinRewards.java
@@ -5,9 +5,18 @@ package com.priyakdey.arrays;
  */
 public class MinRewards {
 
+    // Each student gets at least 1 reward.
+    // Take an array of rewards, init with all values of 1.
+    // Scan from left to right, if score[i] > score[i-1], reward of score[i] must be score[i -1] + 1
+    // Scan from right to left, if score[i] > score[i-1], reward of score[i]
+    // must be Math.max(existing reward[i], score[i -1] + 1)
+
+    // Two passes handle both directions - take max of both constraints.
+    // Sum final array for total rewards.
+
     public static int minRewards(int[] scores) {
         int length = scores.length;
-        if (length < 3 ) return (2 * length) - 1;
+        if (length < 3) return (2 * length) - 1;
 
         int[] arr1 = new int[length];
 
@@ -19,7 +28,7 @@ public class MinRewards {
                 arr1[i] = 1;
             }
         }
-        
+
         for (int i = length - 2; i >= 0; i--) {
             if (scores[i] > scores[i + 1]) {
                 arr1[i] = Math.max(arr1[i], arr1[i + 1] + 1);
